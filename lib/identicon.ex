6a43da61dbbdbe76c963ce5be838fff7638cce7b
Sam <thesamreaves@gmail.com>
2018-05-11 19:28:26 -0400

    Added methods to hash strings and pick a hex code from the hash. Piped together in main function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,15 +4,50 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
+  Main function for Identicon program
 
   ## Examples
 
-      iex> Identicon.hello
-      :world
+      iex> Identicon.main('testing123')
+      [127, 42, 186]
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_string
+    |> pick_color
+  end
+
+  @doc """
+  Hash String
+
+  ## Examples
+
+      iex> Identicon.hash_string('testing123')
+      [127, 42, 186, 186, 66, 48, 97, 197, 9, 244, 146, 61, 208, 75, 108, 241]
+
+  """
+  def hash_string(string) do
+    hex = :crypto.hash(:md5, string)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+
+
+  @doc """
+  Pick Color
+
+  ## Examples
+
+      iex> Identicon.pick_color()
+
+
+  """
+  def pick_color(image) do
+    %Identicon.Image{hex: hex_list} = image
+    [r, g, b | _tail] = hex_list
+
+    [r, g, b]
   end
 end
